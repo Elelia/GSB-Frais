f3d16cd94fa7f5d825c4d37975dd44679f70cec8
Lisa <90413384+Elelia@users.noreply.github.com>
2022-05-29 19:05:42 +0200

    Update class.database.inc.php

--- modele/class.database.inc.php
+++ modele/class.database.inc.php
@@ -11,8 +11,8 @@ class Database
 
     private function __construct()
     {
-    	Database::$monPdo = new PDO('mysql:host=62.23.119.27;port=3333;dbname=gsb_fraislisa', 'Lisa', 'Lisa');
-		//Database::$monPdo = new PDO('mysql:host=192.168.0.15;port=3333;dbname=gsb_fraislisa', 'lisa', 'lisa');
+    	Database::$monPdo = new PDO('mysql:host=******;port=3333;dbname=gsb_fraislisa', 'Lisa', 'Lisa');
+		//Database::$monPdo = new PDO('mysql:host=*****;port=3333;dbname=gsb_fraislisa', 'lisa', 'lisa');
 		//Database::$monPdo = new PDO('pgsql:host=localhost;port=5432;dbname=gsb_fraislisa', 'superuser', 'pinG07@un');
 		//Database::$monPdo = new PDO(Database::$bdd.';'.Database::$serveur.';'.Database::$port, Database::$user, Database::$mdp);
 		Database::$monPdo->query("SET CHARACTER SET utf8");
